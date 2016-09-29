33ce04c88214cf1f596722a7e11e66e56e0e5525
zsenyukt <zsenyukt@gmail.com>
2016-09-29 12:50:58 +0000

    separated Dc motor test files

--- motor2.py
+++ motor2.py
@@ -11,11 +11,11 @@ GPIO.setup(Motor1A,GPIO.OUT)
 GPIO.setup(Motor1B,GPIO.OUT)
 GPIO.setup(Motor1E,GPIO.OUT)
  
-print "Going forwards"
-GPIO.output(Motor1A,GPIO.HIGH)
-GPIO.output(Motor1B,GPIO.LOW)
+ 
+print "Going backwards"
+GPIO.output(Motor1A,GPIO.LOW)
+GPIO.output(Motor1B,GPIO.HIGH)
 GPIO.output(Motor1E,GPIO.HIGH)
-
  
 sleep(5)
  
