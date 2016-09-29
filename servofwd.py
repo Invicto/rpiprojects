5bd8fbd4fdcdbe68825880ff341e45c3f4d17cb7
zsenyukt <zsenyukt@gmail.com>
2016-09-29 13:16:49 +0000

    servo tests

---
+++ servofwd.py
@@ -0,0 +1,14 @@
+from time import sleep
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.OUT)
+
+p = GPIO.PWM(12, 50)  # channel=12 frequency=50Hz
+
+#full left
+print "full left"
+p.start(5)
+sleep(1)
+
+p.stop()
+GPIO.cleanup()
